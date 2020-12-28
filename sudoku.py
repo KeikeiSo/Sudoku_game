315bbfb34502f16502b74fe6761ad36cc240bcae
KeikeiSo <calliasu2018@gmail.com>
2020-12-27 21:10:44 -0500

    Finished Front
    print the sudoku board and restricted the input

--- sudoku.py
+++ sudoku.py
@@ -7,6 +7,7 @@ author: Qiqi Su
 """ Import statements """
 import tkinter as tk
 from PIL import ImageTk, Image
+from create_sudoku import create_sudoku
 
 
 class Sudoku(tk.Frame):
@@ -21,7 +22,7 @@ class Sudoku(tk.Frame):
         self.master.iconphoto(False, icon)
 
         # create canvas
-        self.canvas = tk.Canvas(self, bg="white", width=440, height=500)
+        self.canvas = tk.Canvas(self, bg="white", width=440, height=600)
         self.canvas.pack(fill="both", expand=True)
 
         
@@ -30,29 +31,83 @@ class Sudoku(tk.Frame):
         # create background
         # photo should be in the same file
         openbg = Image.open("green_red_bg.png")
-        bg = ImageTk.PhotoImage(openbg.resize((440, 500)))
-        self.canvas.create_image(0, 0, image=bg, anchor="nw")
+        self.bg = ImageTk.PhotoImage(openbg.resize((440, 600)))
+        self.canvas.create_image(0, 0, image=self.bg, anchor="nw")
 
         # create front title
         self.frontmsg = self.canvas.create_text(225, 200, text="Sudoku",
                    font=("Helvetica", 50), fill = "white")
 
         # create start button
-        """
         self.start_btn = tk.Button(root, text="Start", font=("Helvetica", 30),
                       width=20, fg="white", bg="coral1", command=self.start)
-        self.start_btn_window = self.canvas.create_window(225, 400, width=200, window=start_btn)
-        
-        """
-    def start(self):
-        return
+        self.start_btn_window = self.canvas.create_window(225, 350, width=200,
+                                                          window=self.start_btn)
+
+
+    def bind_entry(self, event):
+        userinput = event.widget.get()
+        if userinput not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            event.widget.delete(0, tk.END)
     
+    def start(self):
+        # delete previous stuffs
+        self.canvas.delete(self.frontmsg)
+        self.start_btn.destroy()
+
+        # make a board
+        opencv = Image.open("canvas.png")
+        self.cv = ImageTk.PhotoImage(opencv)
+        self.canvas.create_image(39, 39, image=self.cv, anchor="nw")
+
+        self.draw_table()
+
+        # generate a sudoku
+        self.sudoku = create_sudoku()
+        self.entries = []
+        self.print_puzzle()
+
+        """
+        # print frame of the sudoku
+        openfr = Image.open("frame.png")
+        self.fr = ImageTk.PhotoImage(openfr)
+        self.canvas.create_image(39, 39, image=self.fr, anchor="nw")
+        """
+        # restrict the user input on entries
+        for l in self.entries:
+            entry = l[2]
+            entry.bind("<Leave>", self.bind_entry)
+
+
+    # helper functions for start
     def draw_table(self):
         for i in range(1, 11):
             coord1 = 40, i*40, 400, i*40
             self.canvas.create_line(coord1, fill = "gray10")
             coord2 = i*40, 40, i*40, 400
             self.canvas.create_line(coord2, fill = "gray10")
+            
+    def print_puzzle(self):
+        for i in range(9):
+            for j in range(9):
+                num = self.sudoku[i][j]
+                if num == 0:
+                    e = tk.Entry(self, font=("Helvetica", 30), width=40)
+                    e["bd"], e["justify"] = 1, 'center'
+                    self.entries.append([i, j, e])
+                    coord = 41+i*40, 41+j*40
+                    self.canvas.create_window(coord, anchor="nw", window=e,
+                                              height=39, width=39)
+                else:
+                    coord = 49+i*40, 40+j*40
+                    self.canvas.create_text(coord, anchor="nw", text=str(num),
+                                            font=("Helvetica", 30))
+
+
+    
+            
+                    
+        
 
     
         
